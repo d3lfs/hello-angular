6487367a17e9e0a0c963ea3c5b6e9d38945796a2
Sly Bacalso <nasa.sly14@gmail.com>
2022-07-10 23:02:51 +0800

    angular testing with eslint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'hello-test';
+export class AppComponent implements OnInit {
+    ngOnInit(): void {
+        console.log('Hello world');
+    }
+    title = 'hello-test';
 }
